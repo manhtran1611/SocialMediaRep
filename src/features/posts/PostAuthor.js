d914eb488152c21abe280439f0fb371e727ef7e3
manhtran1611 <85244387+manhtran1611@users.noreply.github.com>
2021-10-16 10:55:19 +0700

    Build Production

--- src/features/posts/PostAuthor.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { useAppSelector } from "../../app/hooks";
-import { selectUserById } from "../users/usersSlice";
-export const PostAuthor = ({ userId }) => {
-  const author = useAppSelector(selectUserById);
-  return <span>by {author ? author.name : "Unknown author"}</span>;
-};
